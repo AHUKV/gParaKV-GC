a163a364d3f7347b42e8896594ecf8ddb1b40506
AHUKV <ahu2032@gmail.com>
2025-04-21 14:15:43 +0800

    Delete cmake-build-debug directory

--- cmake-build-debug/third_party/benchmark/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/My/ParaKV-GC/third_party/benchmark/src
-# Build directory: /home/ubuntu/My/ParaKV-GC/cmake-build-debug/third_party/benchmark/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
