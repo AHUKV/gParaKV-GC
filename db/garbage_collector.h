dc13236b5bc5cf3ecb8a14528d7928c823f903d5
KV890 <jxxahu@gmail.com>
2025-09-03 23:43:29 +0800

    update

--- db/garbage_collector.h
+++ db/garbage_collector.h
@@ -1,11 +1,10 @@
 
-#pragma
+#pragma once
 
 #include "db/cuda/gpu_sort.cuh"
 #include "db/vlog_reader.h"
 #include <cstdint>
 
-#include "pmem_hashmap.h"
 #include "vlog_writer.h"
 
 namespace leveldb {
@@ -22,12 +21,6 @@ class GarbageCollector {
   ~GarbageCollector() { delete vlog_reader_; }
 
   void SetVlog(uint64_t vlog_number, uint64_t garbage_beg_pos = 0);
-
-  void BeginGarbageCollect(const Slice& file,
-                           const std::vector<uint32_t>& invalid_pos);
-
-  static void BeginMigration(const Slice& file, HashMap& hashMap,
-                             const std::vector<uint32_t>& keyValuesPos);
 
  private:
   uint64_t vlog_number_;
